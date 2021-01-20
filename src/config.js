f6b1b066240455d7d6371c576796db99fd4f8a49
mwgambill <mwgambill@gmail.com>
2021-01-20 09:53:41 -0500

    initial

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: `postgres://fhzdisraldehaj:ab3933bfad49ea8472fea2b285182eff25c6ae8d380cb27b7707538c7f68a76e@ec2-52-72-190-41.compute-1.amazonaws.com:5432/d9603kvrh4sk30`,
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
